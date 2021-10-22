48d23d7c24bb57d0aa0ee4b8c1085a011930b144
Ahmed Jama <ahmedjama@Ahmeds-Air.hitronhub.home>
2021-10-21 18:34:53 -0600

    Added initial lambda function to add car listing to DB

---
+++ carListingLambda.js
@@ -0,0 +1,40 @@
+// Include the AWS SDK module
+const AWS = require('aws-sdk');
+// Instantiate a DynamoDB document client with the SDK
+let dynamodb = new AWS.DynamoDB.DocumentClient();
+// Use built-in module to get current date & time
+let date = new Date();
+// Store date and time in human-readable format in a variable
+let now = date.toISOString();
+// Define handler function, the entry point to our code for the Lambda service
+// We receive the object that triggers the function as a parameter
+exports.handler = async (event) => {
+    // Extract values from event and format as strings
+    let listResponse = JSON.stringify(`New Car Listed: ${event.modelYear} ${event.make} ${event.model}`);
+    let queryParams = {
+        TableName:'listedCars',
+        AttributesToGet: 'carID'
+    };
+    let carID = await dynamodb.get(params).promise();
+    debugger;
+    // Create JSON object with parameters for DynamoDB and store in a variable
+    let params = {
+        TableName:'listedCars',
+        Item: {
+            'carID': name,
+            'ModelYear': event.modelYear,
+            'Make': event.make,
+            'Model': event.model,
+            'dateCreated': now
+        }
+    };
+    // Using await, make sure object writes to DynamoDB table before continuing execution
+    await dynamodb.put(params).promise();
+    // Create a JSON object with our response and store it in a constant
+    const response = {
+        statusCode: 200,
+        body: listResponse
+    };
+    // Return the response constant
+    return response;
+};
